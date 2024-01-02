51848ee2c101cd6f867466b9e4f3576decb320e8
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2024-01-02 15:09:10 -0300

    Finalização parcial do Backend

--- backend/src/main/java/com/api/servico/backend/controller/ServicoController.java
+++ backend/src/main/java/com/api/servico/backend/controller/ServicoController.java
@@ -19,6 +19,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.api.servico.backend.entity.Servico;
 import com.api.servico.backend.service.ServicoService;
 
+/**
+ * Controlador para operações relacionadas à entidade Servico.
+ */
 @RestController
 @RequestMapping(value="/api/servicos")
 public class ServicoController {
@@ -26,30 +29,57 @@ public class ServicoController {
     @Autowired
     private ServicoService servicoService;
     
-	@GetMapping
-	public ResponseEntity<List<Servico>> findAll() {
-		 List<Servico> list = servicoService.buscarTodos();
-		 return ResponseEntity.ok().body(list);
-	}
+    /**
+     * Recupera todos os serviços.
+     *
+     * @return ResponseEntity contendo a lista de todos os serviços.
+     */
+    @GetMapping
+    public ResponseEntity<List<Servico>> findAll() {
+        List<Servico> list = servicoService.buscarTodos();
+        return ResponseEntity.ok().body(list);
+    }
 
-    @GetMapping(value="/{id}")
+    /**
+     * Recupera um serviço pelo ID.
+     *
+     * @param id ID do serviço a ser buscado.
+     * @return ResponseEntity contendo o serviço correspondente ao ID fornecido.
+     */
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Servico> findById(@PathVariable Long id) {
         Servico obj = servicoService.buscarPorId(id);
         return ResponseEntity.ok().body(obj);
     }
 
-    @GetMapping(value="/pagamentopendente")
+    /**
+     * Recupera serviços com pagamento pendente.
+     *
+     * @return ResponseEntity contendo a lista de serviços com pagamento pendente.
+     */
+    @GetMapping(value = "/pagamentopendente")
     public ResponseEntity<List<Servico>> buscarServicosPagamentoPendente() {
-    	List<Servico> list = servicoService.buscarServicosPagamentoPendente();
-    	return ResponseEntity.ok().body(list);
+        List<Servico> list = servicoService.buscarServicosPagamentoPendente();
+        return ResponseEntity.ok().body(list);
     }
 
-    @GetMapping(value="/cancelados")
+    /**
+     * Recupera serviços cancelados.
+     *
+     * @return ResponseEntity contendo a lista de serviços cancelados.
+     */
+    @GetMapping(value = "/cancelados")
     public ResponseEntity<List<Servico>> buscarServicosCancelados() {
         List<Servico> list = servicoService.buscarServicosCancelados();
         return ResponseEntity.ok().body(list);
     }
 
+    /**
+     * Insere um novo serviço.
+     *
+     * @param obj Serviço a ser inserido.
+     * @return ResponseEntity contendo o serviço inserido e a URI para acessar o recurso.
+     */
     @PostMapping
     public ResponseEntity<Servico> insert(@RequestBody Servico obj) {
         obj = servicoService.inserir(obj);
@@ -58,15 +88,39 @@ public class ServicoController {
                 .buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
-    
+
+    /**
+     * Atualiza um serviço existente.
+     *
+     * @param obj Serviço com as alterações a serem aplicadas.
+     * @return O serviço atualizado.
+     */
     @PutMapping
     public Servico update(@RequestBody Servico obj) {
-    	return servicoService.alterar(obj);
+        return servicoService.alterar(obj);
     }
-    
+
+    /**
+     * Exclui um serviço pelo ID.
+     *
+     * @param id ID do serviço a ser excluído.
+     * @return ResponseEntity indicando o sucesso da operação.
+     */
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> excluir(@PathVariable Long id) {
-    	servicoService.excluir(id);
-    	return ResponseEntity.noContent().build();
+        servicoService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * Cancela um serviço pelo ID.
+     *
+     * @param id ID do serviço a ser cancelado.
+     * @return ResponseEntity indicando o sucesso da operação.
+     */
+    @PostMapping(value = "/cancelarservico/{id}")
+    public ResponseEntity<Void> cancelarServico(@PathVariable Long id) {
+        servicoService.cancelarServico(id);
+        return ResponseEntity.ok().build();
     }
 }
--- backend/src/main/java/com/api/servico/backend/service/ServicoService.java
+++ backend/src/main/java/com/api/servico/backend/service/ServicoService.java
@@ -15,6 +15,9 @@ import com.api.servico.backend.service.exceptions.DatabaseException;
 import com.api.servico.backend.service.exceptions.ResourceNotFoundException;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+/**
+ * Serviço para operações relacionadas à entidade Servico.
+ */
 @Service
 @JsonIgnoreProperties({"hibernateLazyInitializer"})
 public class ServicoService {
@@ -22,23 +25,51 @@ public class ServicoService {
     @Autowired
     private ServicoRepository servicoRepository;
 
+    /**
+     * Busca todos os serviços.
+     *
+     * @return Lista de todos os serviços.
+     */
     public List<Servico> buscarTodos() {
         return servicoRepository.findAll();
     }
 
+    /**
+     * Busca um serviço pelo ID.
+     *
+     * @param id ID do serviço a ser buscado.
+     * @return Serviço correspondente ao ID fornecido.
+     * @throws ResourceNotFoundException Se o serviço não for encontrado.
+     */
     public Servico buscarPorId(Long id) {
         Optional<Servico> obj = servicoRepository.findById(id);
         return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
+    /**
+     * Busca serviços com pagamento pendente.
+     *
+     * @return Lista de serviços com pagamento pendente.
+     */
     public List<Servico> buscarServicosPagamentoPendente() {
         return servicoRepository.buscarServicosPagamentoPendente();
     }
 
+    /**
+     * Busca serviços cancelados.
+     *
+     * @return Lista de serviços cancelados.
+     */
     public List<Servico> buscarServicosCancelados() {
         return servicoRepository.buscarServicosCancelados();
     }
 
+    /**
+     * Insere um novo serviço.
+     *
+     * @param servico Serviço a ser inserido.
+     * @return Serviço inserido.
+     */
     public Servico inserir(Servico servico) {
         if (servico.getValorPago() == null || servico.getValorPago() == 0 || servico.getDataPagamento() == null) {
             servico.setStatus("pendente");
@@ -48,6 +79,12 @@ public class ServicoService {
         return servicoRepository.save(servico);
     }
 
+    /**
+     * Altera um serviço existente.
+     *
+     * @param obj Serviço com as alterações a serem aplicadas.
+     * @return Serviço alterado.
+     */
     public Servico alterar(Servico obj) {
     	 Servico servicoExistente = servicoRepository.findById(obj.getId()).orElse(null);
     	 if (servicoExistente != null) {
@@ -66,7 +103,26 @@ public class ServicoService {
     	 }
          return servicoRepository.save(obj);
     }
+    
 
+    /**
+     * Cancela um serviço pelo ID.
+     *
+     * @param id ID do serviço a ser cancelado.
+     */
+    public void cancelarServico(Long id) {
+    	Servico servico = servicoRepository.findById(id).get(); // Recupero os dados do banco
+    	servico.setStatus("cancelado");							// Alteramos o Status
+    	servicoRepository.save(servico);						// Atualizamos
+    }
+
+    /**
+     * Exclui um serviço pelo ID.
+     *
+     * @param id ID do serviço a ser excluído.
+     * @throws ResourceNotFoundException Se o serviço não for encontrado.
+     * @throws DatabaseException         Se ocorrer um erro de integridade no banco de dados.
+     */
     public void excluir(Long id) {
         try {
             servicoRepository.deleteById(id);
